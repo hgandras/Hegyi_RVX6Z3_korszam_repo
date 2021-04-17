ab0b66177ecd2b288dd113b463d704dc66337bf6
hgandras <hgandras@gmail.com>
2021-04-17 16:34:17 +0200

    'added output file'

--- Der-int/Kotel_hossz.cpp
+++ Der-int/Kotel_hossz.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Integral.h"
 #include <math.h>
+#include <fstream>
 
 using namespace std;
 
@@ -12,30 +13,49 @@ int main(){
     const float h=35;
     const float a=200;
     float delta;
+    float perc;
 
-    cout<<"Kérek egy delta hibatagot:"<<endl;
+    cout<<"Kérek egy derivált delta hibatagot:";
     cin >> delta;
+    cout<<"Kérek egy integrál százalékos hibát (0 és 1 között):";
+    cin>>perc;
 
     
     float deriv_2_max=0;
+    float deriv_4_max=0;
     float deriv_1=0;
+    float deriv_2=0;
+    float deriv_3=0;
+    float deriv_4=0;
 
-    for(float i = a/200; i < 200; i+=a/200){
+    for(float i = a/10000; i < 200; i+=a/10000){
         deriv_1=hatod_deriv(i,F,q,h,a,delta);
-        cout<<deriv_1<<'\n';
-        if (abs(hatod_deriv(deriv_1,F,q,h,a,delta))>deriv_2_max){
-            deriv_2_max=hatod_deriv(deriv_1,F,q,h,a,delta);
+        deriv_2=hatod_deriv(deriv_1,F,q,h,a,delta);
+        deriv_3=hatod_deriv(deriv_2,F,q,h,a,delta);
+        deriv_4=hatod_deriv(deriv_3,F,q,h,a,delta);
+        if (abs(deriv_2)>deriv_2_max){
+            deriv_2_max=abs(deriv_2);
+        }
+        if (abs(deriv_4)>deriv_4_max){
+            deriv_4_max=abs(deriv_4);
         }
     }
 
-    float n_kozep=sqrt(deriv_2_max*(a*a*a)/(24*0.01));
-    float n_trapez=sqrt(deriv_2_max*(a*a*a)/(12*0.01));
+    float n_kozep=sqrt(deriv_2_max*(a*a*a)/(24*perc));
+    float n_trapez=sqrt(deriv_2_max*(a*a*a)/(12*perc));
+    //float n_simp=pow(a*a*a*a*a*deriv_4_max/(2880*perc),1/4);
+    float n_simp_2=(2*n_kozep+n_trapez)/3;
 
-    cout<<kozeppontos(n_kozep,F,q,h,a,delta)<<endl;
+    float len_kozeppontos=kozeppontos(n_kozep,F,q,h,a,delta);
+    float len_trapez=trapez(n_trapez,F,q,h,a,delta);
+    float len_simp=harmad_simpson(n_simp_2,F,q,h,a,delta);
 
-    cout<<trapez(n_trapez,F,q,h,a,delta)<<endl;
+    ofstream f;
+    f.open("output.txt");
+    f<<len_kozeppontos<<','<<len_trapez<<','<<len_simp<<endl;
+    f<<"Megjegyzés: 4. deriváltnál nagyon kicsi érték jött ki, ezért a harmad Simpson módszer setében n-re 1 jött ki,\n"<<"amire nagyon nem volt pontos az eredmény. Emiatt ahhoz az n-et a (2*középpontos+trapéz)/3 szerint számoltam.";
+    f.close();
 
-    cout<<harmad_simpson(1000,F,q,h,a,delta);
 
 
 
