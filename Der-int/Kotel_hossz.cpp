5f566e08e7efe25e70af1efa982eccea9bac6220
hgandras <hgandras@gmail.com>
2021-04-17 14:14:54 +0200

    'added errors'

--- Der-int/Kotel_hossz.cpp
+++ Der-int/Kotel_hossz.cpp
@@ -21,11 +21,19 @@ int main(){
     float deriv_1=0;
 
     for(float i = a/200; i < 200; i+=a/200){
-        cout<<hatod_deriv(i,F,q,h,a,delta)<<'\n';
+        deriv_1=hatod_deriv(i,F,q,h,a,delta);
+        cout<<deriv_1<<'\n';
+        if (abs(hatod_deriv(deriv_1,F,q,h,a,delta))>deriv_2_max){
+            deriv_2_max=hatod_deriv(deriv_1,F,q,h,a,delta);
+        }
     }
-    cout<<kozeppontos(1000,F,q,h,a,delta)<<endl;
 
-    cout<<trapez(1000,F,q,h,a,delta)<<endl;
+    float n_kozep=sqrt(deriv_2_max*(a*a*a)/(24*0.01));
+    float n_trapez=sqrt(deriv_2_max*(a*a*a)/(12*0.01));
+
+    cout<<kozeppontos(n_kozep,F,q,h,a,delta)<<endl;
+
+    cout<<trapez(n_trapez,F,q,h,a,delta)<<endl;
 
     cout<<harmad_simpson(1000,F,q,h,a,delta);
 
