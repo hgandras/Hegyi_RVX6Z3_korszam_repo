7ef8f7736fe3c51e055f3a767e4ede5775be8308
hgandras <hgandras@gmail.com>
2021-04-24 15:27:31 +0200

    RK+analitikus

--- 4.feladat/Deriv.cpp
+++ 4.feladat/Deriv.cpp
@@ -12,18 +12,20 @@ auto func(T y){
 }
 
 template<typename T>
-vector analytic(){
+vector<T> analytic(vector<T> y ,T t0,T full_t,T t_step){
+    for (double i=t0;i<=full_t;i+=t_step){
+        y.push_back(atan(i));
+    }
 
-    return ;
+    return y;
 }
 
 
 template<typename T>
 vector<T> Newton(vector<T> y,T (*f)(T),T y0 ,T t0,T full_t,T t_step){
-    int i=1;
     y.push_back(y0);
-    for (int i=1;i<=(full_t-t0)/t_step;i+=1){
-        y.push_back(y[i-1]+f(y[i])*t_step);
+    for (int i=1;i<=(full_t-t0)/t_step;i++){
+        y.push_back(y[i-1]+f(y[i-1])*t_step);
     }
     return y;
 }
@@ -32,7 +34,14 @@ template<typename T>
 vector<T> RK4(vector<T> y,T (*f)(T),T y0,T t0,T full_t,T t_step){
     double k1,k2,k3,k4;
     y.push_back(y0);
-    for(int i=1;i<=(full_t-t0)/t_step;i+=1)
+    for(int i=1;i<=(full_t-t0)/t_step;i++){
+        k1=f(y[i-1]);
+        k2=f(y[i-1]+t_step*k1/2);
+        k3=f(y[i-1]+t_step*k2/2);
+        k4=f(y[i-1]+t_step*k3);
+        y.push_back(y[i-1]+t_step*(k1+2*k2+2*k3+k4)/6);
+        
+    }
 
 
     return y;
@@ -56,9 +65,12 @@ int main(){
     
     vector<double> res;
 
-    vector<double> results=Newton(res,func,y0,t0,t_full,t_step);
-    for(int i=0;i<results.size();i++)
-        cout<<results[i]<<endl;
+    vector<double> results_N=Newton(outp,func,y0,t0,t_full,t_step);
+    vector<double> results_RK=RK4(outp,func,y0,t0,t_full,t_step);
+    vector<double> results_analytic=analytic(outp,t0,t_full,t_step);
+    for(int i=0;i<results_N.size();i++)
+        cout<<results_N[i]<<','<<results_RK[i]<<','<<results_analytic[i]<<endl;
+
 
 
 
