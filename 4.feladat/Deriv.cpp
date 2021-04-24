75b7bc696f62b9d8e19970c04985ed99c8cd219b
hgandras <hgandras@gmail.com>
2021-04-24 14:23:44 +0200

    Newton formula

--- 4.feladat/Deriv.cpp
+++ 4.feladat/Deriv.cpp
@@ -6,23 +6,61 @@
 using namespace std;
 
 
-double Newton(double y0,double t0,double full_t,double t_step){
+template<typename T>
+auto func(T y){
+    return 1+pow(y,2);
+}
 
-    return 0;
+template<typename T>
+vector analytic(){
+
+    return ;
 }
 
 
-double RK4(double y0,double t0,double full_t,double t_step){
-
-
-    return 0;
+template<typename T>
+vector<T> Newton(vector<T> y,T (*f)(T),T y0 ,T t0,T full_t,T t_step){
+    int i=1;
+    y.push_back(y0);
+    for (int i=1;i<=(full_t-t0)/t_step;i+=1){
+        y.push_back(y[i-1]+f(y[i])*t_step);
+    }
+    return y;
 }
 
+template<typename T>
+vector<T> RK4(vector<T> y,T (*f)(T),T y0,T t0,T full_t,T t_step){
+    double k1,k2,k3,k4;
+    y.push_back(y0);
+    for(int i=1;i<=(full_t-t0)/t_step;i+=1)
 
+
+    return y;
+}
 
 
 
 int main(){
     
+    double t0,t_full,t_step,y0;
+    vector<double> outp;
+
+    cout<<"t0: ";
+    cin>>t0;
+    cout<<"y0: ";
+    cin>>y0;
+    cout<<"t_step: ";
+    cin>>t_step;
+    cout<<"t_full: ";
+    cin>>t_full;
+    
+    vector<double> res;
+
+    vector<double> results=Newton(res,func,y0,t0,t_full,t_step);
+    for(int i=0;i<results.size();i++)
+        cout<<results[i]<<endl;
+
+
+
     return 0;
 }
